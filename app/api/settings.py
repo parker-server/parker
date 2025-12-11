b296a00ba435ea399ea9df8d69d68e1a0a1be077
Breen Young <breen.young@gmail.com>
2025-12-10 22:28:45 -0400

    add endpoint to retrieve a single settings value and filter thru a whitelist to see if it's a public accessible one.  Alter dynamic background routine to use this new endpoint

--- app/api/settings.py
+++ app/api/settings.py
@@ -1,17 +1,29 @@
 from fastapi import APIRouter, HTTPException
 from typing import Dict, List, Any
-from app.api.deps import SessionDep, AdminUser
+from app.api.deps import SessionDep, AdminUser, CurrentUser
 from app.services.settings_service import SettingsService
 from app.services.scheduler import scheduler_service
 from app.schemas.setting import SettingUpdate, SettingResponse
 
 router = APIRouter()
 
+PUBLIC_KEYS = [ "ui.background_style" ]
+
 @router.get("/", response_model=Dict[str, List[SettingResponse]], status_code=200, name="list")
-def get_settings(db: SessionDep, admin: AdminUser):
+def get_settings(db: SessionDep):
     """Get all settings grouped by category"""
     svc = SettingsService(db)
     return svc.get_all_grouped()
+
+@router.get("/{key}", name="value")
+def get_setting_value(key: str, db: SessionDep, current_user: CurrentUser):
+
+    # Return setting value if user is an admin or the key is in allowable public setting keys
+    if current_user.is_superuser or key in PUBLIC_KEYS:
+        svc = SettingsService(db)
+        return { "value": svc.get(key) }
+
+    return None
 
 @router.patch("/{key}", tags=["admin"], name="update")
 def update_setting(key: str, payload: SettingUpdate, db: SessionDep, admin: AdminUser):
