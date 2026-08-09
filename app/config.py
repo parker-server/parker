fbacfebf260848b58989239306522b8097636c67
Breen Young <breen.young@gmail.com>
2026-08-09 13:49:03 -0300

    Prepare 0.1.29 release

--- app/config.py
+++ app/config.py
@@ -20,7 +20,7 @@ def _split_comma_list(value: str) -> list[str]:
 
 class Settings(BaseSettings):
     app_name: ClassVar[str] = "Parker"
-    version: ClassVar[str] = "0.1.28"
+    version: ClassVar[str] = "0.1.29"
     
     database_url: str = "sqlite:///./storage/database/comics.db"
     #database_url: str = "sqlite:///./storage/database/temp.db"
